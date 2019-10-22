7dd4221da4ffd8873684180030b13842f6dae36d
Douglas Ardila Garcés <dag1702@hotmail.com>
2019-10-22 18:45:58 -0500

    leer varios registros

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import cop_prediction as cop
 import clustering
-import pandas as pd
+import dataset
 
 # declare constants
 HOST = '0.0.0.0'
@@ -10,41 +10,15 @@ PORT = 8081
 
 # initialize flask application
 app = Flask(__name__)
+
 CORS(app)
+
 # Datos
-columns = ['POTENCIA TRAFO 2', 'POTENCIA TRAFO 3', 'POTENCIA TRAFO 4', 'POTENCIA TRAFO 5', 'POTENCIA MEDIA CONECTADA',
-           'PERIODO P6', 'CONTROL FRÍO', 'CONTROL CALOR', 'CAPACIDAD BOMBA CALOR FELIPE %', 'CAPACIDAD BOMBA CALOR CARLOS %',
-           'POTENCIA BOMBA CALOR FELIPE', 'POTENCIA BOMBA CALOR CARLOS', 'TEMPERATURA AMBIENTE BOMBA CALOR CARLOS',
-           'TEMPERATURA AMBIENTE BOMBA CALOR FELIPE', 'TEMPERATURA EXTERIOR', 'CAPACIDAD GRUPO DE FRÍO 1', 'CAPACIDAD GRUPO DE FRÍO 2',
-           'POTENCIA GRUPO FRÍO 1', 'POTENCIA GRUPO FRÍO 2', 'ENTRADA AGUA A TORRE 1', 'SALIDA AGUA TORRE 1', 'ENTRADA AGUA A TORRE 2',
-           'SALIDA AGUA TORRE 2', 'C_O_P MÁQUINA GRUPO FRÍO 1', 'C_O_P MÁQUINA GRUPO FRÍO 2', 'C_O_P BOMBA CALOR CARLOS',
-           'C_O_P BOMBA CALOR FELIPE', 	 'TEMPERATURA SALIDA BOMBA CALOR CARLOS', 'TEMPERATURA SALIDA BOMBA CALOR FELIPE',
-           'KILO CALORÍAS GENERADAS BOMBA CALOR CARLOS', 'KILO CALORÍAS GENERADAS BOMBA CALOR FELIPE',
-           'KIGO FRIGORÍAS GENERADAS GRUPO DE FRÍO 1', 'KIGO FRIGORÍAS GENERADAS GRUPO DE FRÍO 2', 'VÁLVULA BY PASS SECUNDARIO FRÍO',
-           'TEMPERATURA CONTROL DE BY PASS SECUNDARIO', 'SECUNDARIO FRÍO 1', 'SECUNDARIO FRÍO 2', 'SECUNDARIO FRÍO 3',
-           'IMPULSIÓN SECUNDARIO CALOR', 'MODO INVIERNO BC1', 'MODO INVIERNO BC2', 'SECUNDARIO CALOR 1', 'SECUNDARIO CALOR 2',
-           'SECUNDARIO CALOR 3' ]
-diccionario = {'POTENCIA TRAFO 2': 1.48, 'POTENCIA TRAFO 3': 4.79, 'POTENCIA TRAFO 4': 269.04, 	'POTENCIA TRAFO 5': 393.82,
-               'POTENCIA MEDIA CONECTADA': 715.97, 'PERIODO P6': 1, 'CONTROL FRÍO': 13.7, 'CONTROL CALOR': 0,
-               'CAPACIDAD BOMBA CALOR FELIPE %': 0, 'CAPACIDAD BOMBA CALOR CARLOS %': 31.87, 'POTENCIA BOMBA CALOR FELIPE': 0.64,
-               'POTENCIA BOMBA CALOR CARLOS': 49.47, 'TEMPERATURA AMBIENTE BOMBA CALOR CARLOS': 23.28,
-               'TEMPERATURA AMBIENTE BOMBA CALOR FELIPE': 23.83, 'TEMPERATURA EXTERIOR': 0, 'CAPACIDAD GRUPO DE FRÍO 1': 1.33,
-               'CAPACIDAD GRUPO DE FRÍO 2': 84, 'POTENCIA GRUPO FRÍO 1': 1.38, 'POTENCIA GRUPO FRÍO 2': 63.34,
-               'ENTRADA AGUA A TORRE 1': 24.21, 'SALIDA AGUA TORRE 1': 23.99, 'ENTRADA AGUA A TORRE 2': 30.67,
-               'SALIDA AGUA TORRE 2': 25.93, 'C_O_P MÁQUINA GRUPO FRÍO 1': 0.02, 'C_O_P MÁQUINA GRUPO FRÍO 2': 4.28,
-               'C_O_P BOMBA CALOR CARLOS': 4.2, 'C_O_P BOMBA CALOR FELIPE': 0, 'TEMPERATURA SALIDA BOMBA CALOR CARLOS': 44.05,
-               'TEMPERATURA SALIDA BOMBA CALOR FELIPE': 23.11, 'KILO CALORÍAS GENERADAS BOMBA CALOR CARLOS': 174156.64,
-               'KILO CALORÍAS GENERADAS BOMBA CALOR FELIPE': 0, 'KIGO FRIGORÍAS GENERADAS GRUPO DE FRÍO 1': 10349.98,
-               'KIGO FRIGORÍAS GENERADAS GRUPO DE FRÍO 2': 236150, 'VÁLVULA BY PASS SECUNDARIO FRÍO': 0,
-               'TEMPERATURA CONTROL DE BY PASS SECUNDARIO': 0, 'SECUNDARIO FRÍO 1': 0, 'SECUNDARIO FRÍO 2': 0,
-               'SECUNDARIO FRÍO 3': 0, 'IMPULSIÓN SECUNDARIO CALOR': 0, 'MODO INVIERNO BC1': 0, 'MODO INVIERNO BC2': 0,
-               'SECUNDARIO CALOR 1': 0, 	 'SECUNDARIO CALOR 2': 0, 'SECUNDARIO CALOR 3': 0}
-df = pd.DataFrame(columns=columns)
-df = df.append(diccionario, ignore_index=True)
-df["POTENCIA TERMICA BOMBA CALOR CARLOS"] = df["KILO CALORÍAS GENERADAS BOMBA CALOR CARLOS"] * 0.001163
-df["POTENCIA TERMICA BOMBA CALOR FELIPE"] = df["KILO CALORÍAS GENERADAS BOMBA CALOR FELIPE"] * 0.001163
-df["POTENCIA TERMICA GRUPO FRIO 1"] = df["KIGO FRIGORÍAS GENERADAS GRUPO DE FRÍO 1"] * 0.001163
-df["POTENCIA TERMICA GRUPO FRIO 2"] = df["KIGO FRIGORÍAS GENERADAS GRUPO DE FRÍO 2"] * 0.001163
+obj = dataset.Dataset()
+data = obj.read_dataset_lectura()
+dataFrio1 = data
+dataCarlos = data
+print("Dataset leido")
 
 # Prediccion
 @app.route('/api/train_frio_1', methods=['POST'])
@@ -55,22 +29,32 @@ def train_frio_1():
 
 @app.route('/api/predict_frio_1', methods=['POST'])
 def predict_frio_1():
-    # get data to be predicted
-    X = [[float(df['POTENCIA GRUPO FRÍO 1']), float(df['POTENCIA TERMICA GRUPO FRIO 1']),
-          float(df['TEMPERATURA EXTERIOR'])]]
     cop_model = cop.COP()
-    prediction_ = cop_model.predict_grupo_frio_1(X)
-    prediction = float(prediction_[0])
-    valorReal = float(df['C_O_P MÁQUINA GRUPO FRÍO 1'].iloc[0])
-    print('COP frio 1 predicho:', prediction)
-    print('COP frio 1 leido:', valorReal)
-    # COP malo --> diagnostico
-    if (((valorReal <= 3.5) or (valorReal >= 4.5)) and ((prediction <= 3.5) or (prediction >= 4.5))
-            or (valorReal <= prediction - 0.5) or (valorReal >= prediction + 0.5)):
-        kmeans_ = clustering.KMeans_()
-        kmeans_prediction = kmeans_.predict_frio_1(X)
-        print('Diagnostico frio 1:', kmeans_prediction)
-    return jsonify({'registro': "todos las variables + el diagnostico"})
+    diccionario = {}
+    for i in range(1, 10):
+        df = dataFrio1.iloc[i]
+        # get data to be predicted
+        X = [[float(df['POTENCIA GRUPO FRÍO 1']), float(df['POTENCIA TERMICA GRUPO FRIO 1']),
+              float(df['TEMPERATURA EXTERIOR'])]]
+        prediction_ = cop_model.predict_grupo_frio_1(X)
+        prediction = float(prediction_[0])
+        valorReal = float(df['C_O_P MÁQUINA GRUPO FRÍO 1'])
+        print('COP frio 1 predicho:', prediction)
+        print('COP frio 1 leido:', valorReal)
+        # COP malo --> diagnostico
+        kmeans_prediction = ' '
+        if (((valorReal <= 3.5) or (valorReal >= 4.5)) and ((prediction <= 3.5) or (prediction >= 4.5))
+                or (valorReal <= prediction - 0.5) or (valorReal >= prediction + 0.5)):
+            kmeans_ = clustering.KMeans_()
+            kmeans_prediction = kmeans_.predict_frio_1(X)
+            print('Diagnostico frio 1:', kmeans_prediction)
+        df['Diagnostico'] = kmeans_prediction
+        df['C_O_P MÁQUINA GRUPO FRÍO 1 PREDICHO'] = prediction
+        registro_dict = df.to_dict()
+        var = 'registro_' + str(i)
+        diccionario[var] = registro_dict
+    dataFrio1.drop(range(1, 10), axis=0)
+    return jsonify(diccionario)
 
 @app.route('/api/train_carlos', methods=['POST'])
 def train_carlos():
--- backend/dataset.py
+++ backend/dataset.py
@@ -4,7 +4,7 @@ import pandas as pd
 class Dataset:
 
     def read_dataset(self):
-        data = pd.read_excel("./Datasets/HVAC.xlsx", "HISTORICO_DATOS", index_col=0)
+        data = pd.read_excel("../Datasets/HVAC.xlsx", "HISTORICO_DATOS")
         variables_a_eliminar = ["C_O_P_ BOMBA CALOR FELIPE", "C_O_P_ BOMBA CALOR CARLOS",
                                 "C_O_P_ INSTALACIÓN GRUPO FRÍO 1", "C_O_P_ INSTALACÍON GRUPO FRÍO 2",
                                 "ORDEN", "VÁLVULA BY PASS SECUNDARIO FRÍO",
@@ -25,7 +25,28 @@ class Dataset:
         data["POTENCIA TERMICA GRUPO FRIO 2"] = data["KIGO FRIGORÍAS GENERADAS GRUPO DE FRÍO 2"] * 0.001163
         return data
     
-        def read_dataset_limpio(self):
-            data = pd.read_excel("../Datasets/HVAC_limpio.xlsx", "HVAC_limpio", index_col=0)
-            data['Fecha- hora de lectura'] = (data['Fecha- hora de lectura'] - data['Fecha- hora de lectura'].min()) / np.timedelta64(1, 'D')
-            return data
+    def read_dataset_limpio(self):
+        data = pd.read_excel("../Datasets/HVAC_limpio.xlsx", "HVAC_limpio", index_col=0)
+        data['Fecha- hora de lectura'] = (data['Fecha- hora de lectura'] - data['Fecha- hora de lectura'].min()) / np.timedelta64(1, 'D')
+        return data
+
+    def read_dataset_lectura(self):
+        data = pd.read_excel("../Datasets/HVAC.xlsx", "HISTORICO_DATOS")
+        variables_a_eliminar = ["C_O_P_ BOMBA CALOR FELIPE", "C_O_P_ BOMBA CALOR CARLOS",
+                                "C_O_P_ INSTALACIÓN GRUPO FRÍO 1", "C_O_P_ INSTALACÍON GRUPO FRÍO 2",
+                                "ORDEN", "VÁLVULA BY PASS SECUNDARIO FRÍO",
+                                "TEMPERATURA CONTROL DE BY PASS SECUNDARIO", "SECUNDARIO FRÍO 1",
+                                "SECUNDARIO FRÍO 2", "SECUNDARIO FRÍO 3", "MODO INVIERNO BC1",
+                                "MODO INVIERNO BC2", "PERIODO P6", "CONTROL CALOR",
+                                "CAPACIDAD BOMBA CALOR FELIPE %", "CAPACIDAD BOMBA CALOR CARLOS %",
+                                "CAPACIDAD GRUPO DE FRÍO 1", " CAPACIDAD GRUPO DE FRÍO 2",
+                                "IMPULSIÓN SECUNDARIO CALOR", "SECUNDARIO CALOR 1",
+                                "SECUNDARIO CALOR 2", "SECUNDARIO CALOR 3"]
+        lista_variables = data.columns.values.tolist()
+        subLista = [x for x in lista_variables if x not in variables_a_eliminar]
+        data = data[subLista]
+        data["POTENCIA TERMICA BOMBA CALOR CARLOS"] = data["KILO CALORÍAS GENERADAS BOMBA CALOR CARLOS"] * 0.001163
+        data["POTENCIA TERMICA BOMBA CALOR FELIPE"] = data["KILO CALORÍAS GENERADAS BOMBA CALOR FELIPE"] * 0.001163
+        data["POTENCIA TERMICA GRUPO FRIO 1"] = data["KIGO FRIGORÍAS GENERADAS GRUPO DE FRÍO 1"] * 0.001163
+        data["POTENCIA TERMICA GRUPO FRIO 2"] = data["KIGO FRIGORÍAS GENERADAS GRUPO DE FRÍO 2"] * 0.001163
+        return data
