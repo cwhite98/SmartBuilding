ada0d69e07b0d16a282cd1084ffe3f448a06a2ce
Douglas Ardila Garcés <dag1702@hotmail.com>
2019-10-24 17:24:05 -0500

    todo del back de cop grupo frio 2

--- backend/clustering.py
+++ backend/clustering.py
@@ -35,6 +35,36 @@ class KMeans_:
                 diagnostico = 'La TEMPERATURA EXTERIOR esta generando una anomalia en el climatizador.'
         return diagnostico
 
+    def kmeans_frio_2(self):
+        obj = dataset.Dataset()
+        data = obj.read_dataset()
+        dataFrio2 = data[['POTENCIA GRUPO FRÍO 2', 'POTENCIA TERMICA GRUPO FRIO 2', 'TEMPERATURA EXTERIOR']]
+        kmeans = KMeans(4)
+        kmeans.fit(dataFrio2)
+        joblib.dump(kmeans, 'kmeans_frio_2.pkl')
+        centroids = kmeans.cluster_centers_
+        return str(centroids)
+
+    def predict_frio_2(self, X):
+        kmeans = joblib.load('kmeans_frio_2.pkl')
+        clusters = kmeans.predict(X)
+        centroids = kmeans.cluster_centers_
+        diagnostico = 'COP malo, pero no sabemos la razon'
+        for cluster in clusters:
+            minPotenciaFrio2 = 0
+            maxPotenciaFrio2 = 27
+            minPotenciaTermicaFrio2 = 0
+            maxPotenciaTermicaFrio2 = 97
+            minTempExterior = 10.102595
+            maxTempExterior = 27.313549000000002
+            if ((centroids[cluster][0] < minPotenciaFrio2) and (centroids[cluster][0] >= maxPotenciaFrio2)):
+                diagnostico = 'Revisar la POTENCIA GRUPO FRÍO 2.'
+            if ((centroids[cluster][1] < minPotenciaTermicaFrio2) and (centroids[cluster][1] >= maxPotenciaTermicaFrio2)):
+                diagnostico = 'Revisar la POTENCIA TERMICA GRUPO FRIO 2.'
+            if ((centroids[cluster][2] < minTempExterior) and (centroids[cluster][2] >= maxTempExterior)):
+                diagnostico = 'La TEMPERATURA EXTERIOR esta generando una anomalia en el climatizador.'
+        return diagnostico
+
     def kmeans_carlos(self):
         obj = dataset.Dataset()
         data = obj.read_dataset()
